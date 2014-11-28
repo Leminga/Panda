7985bc44223a555d06aa950bd6d1c2e6a955cccc
marku_000 <marku_000@192.168.29.198>
2014-11-28 12:36:14 +0100

    next try

--- build.sbt
+++ build.sbt
@@ -13,19 +13,7 @@ libraryDependencies ++= Seq(
   javaWs,
   "net.sourceforge.jtds" % "jtds" % "1.2" ,
   "mysql" % "mysql-connector-java" % "5.1.18",
-  "org.activiti" % "activiti-engine" % "5.14",
-  "be.objectify" %% "deadbolt-java" % "2.3.0-RC1",
-  "com.feth"      %%  "play-authenticate" % "0.6.1-SNAPSHOT"
+  "org.activiti" % "activiti-engine" % "5.14" 
 )
 
-resolvers ++=Seq(
-  Resolver.url("Objectify Play Repository (release)", url("http://schaloner.github.com/releases/")),
-  Resolver.url("Objectify Play Repository (snapshot)", url("http://schaloner.github.com/snapshots/")),
-  "play-easymail (release)" at "http://joscha.github.io/play-easymail/repo/releases/",
-  "play-easymail (snapshot)" at "http://joscha.github.io/play-easymail/repo/snapshots/",
-  "play-authenticate (release)" at "http://joscha.github.io/play-authenticate/repo/releases/",
-  "play-authenticate (snapshot)" at "http://joscha.github.io/play-authenticate/repo/snapshots/"
-)
-
-lazy val root = (project in file("."))
-.enablePlugins(PlayJava)
+lazy val root = (project in file(".")).enablePlugins(PlayJava)
--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,9 +3,50 @@
 
 # --- !Ups
 
+create table connection (
+  id                        bigint auto_increment not null,
+  connection_type_de        varchar(255),
+  connection_type_en        varchar(255),
+  constraint pk_connection primary key (id))
+;
+
 create table contact (
-  connection_type           integer,
-  constraint ck_contact_connection_type check (connection_type in (0,1)))
+  connection_type_de        integer,
+  connection_type_en        integer,
+  constraint ck_contact_connection_type_de check (connection_type_de in (0,1)),
+  constraint ck_contact_connection_type_en check (connection_type_en in ()))
+;
+
+create table degree (
+  id                        bigint auto_increment not null,
+  degree_de                 varchar(255),
+  degree_en                 varchar(255),
+  constraint pk_degree primary key (id))
+;
+
+create table educationlevel (
+  id                        bigint auto_increment not null,
+  education                 varchar(255),
+  constraint pk_educationlevel primary key (id))
+;
+
+create table emergency_contact (
+  emergency_surname         varchar(255),
+  emergency_name            varchar(255))
+;
+
+create table emergency_relation (
+  id                        bigint auto_increment not null,
+  emergency_relation_de     varchar(255),
+  emergency_relation_en     varchar(255),
+  constraint pk_emergency_relation primary key (id))
+;
+
+create table faculty (
+  id                        bigint auto_increment not null,
+  faculty_de                varchar(255),
+  faculty_en                varchar(255),
+  constraint pk_faculty primary key (id))
 ;
 
 create table human (
@@ -15,6 +56,26 @@ create table human (
   date_of_birth             datetime)
 ;
 
+create table sex (
+  id                        bigint auto_increment not null,
+  sex_de                    varchar(255),
+  sex_en                    varchar(255),
+  constraint pk_sex primary key (id))
+;
+
+create table sizes (
+  jacket_size               integer,
+  trousers_size             integer,
+  shoe_size                 integer)
+;
+
+create table translation (
+  id                        bigint auto_increment not null,
+  german                    varchar(255),
+  english                   varchar(255),
+  constraint pk_translation primary key (id))
+;
+
 create table volunteer (
   id                        bigint auto_increment not null,
   surname                   varchar(255),
@@ -22,6 +83,8 @@ create table volunteer (
   sex                       tinyint(1) default 0,
   date_of_birth             datetime,
   password                  varchar(255),
+  id_text_boxes             bigint,
+  id_event_comment          bigint,
   constraint pk_volunteer primary key (id))
 ;
 
@@ -32,9 +95,27 @@ create table volunteer (
 
 SET FOREIGN_KEY_CHECKS=0;
 
+drop table connection;
+
 drop table contact;
 
+drop table degree;
+
+drop table educationlevel;
+
+drop table emergency_contact;
+
+drop table emergency_relation;
+
+drop table faculty;
+
 drop table human;
+
+drop table sex;
+
+drop table sizes;
+
+drop table translation;
 
 drop table volunteer;
 
