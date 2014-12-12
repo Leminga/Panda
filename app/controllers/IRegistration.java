2388480128c248c60a3c679dcfdf6eeb750ff6cd
Michael Bredel <michael.bredel@cern.ch>
2014-12-12 10:46:09 +0100

    Removed javafx.

--- app/controllers/IRegistration.java
+++ app/controllers/IRegistration.java
@@ -2,27 +2,27 @@ package controllers;
 
 import java.util.Observer;
 
-import javafx.beans.InvalidationListener;
-import javafx.beans.Observable;
+//import javafx.beans.InvalidationListener;
+//import javafx.beans.Observable;
 
-public class IRegistration implements Observer, Observable {
+public class IRegistration implements Observer {
 
-	@Override
-	public void addListener(InvalidationListener arg0) {
-		// TODO Auto-generated method stub
+//	@Override
+//	public void addListener(InvalidationListener arg0) {
+//		// TODO Auto-generated method stub
+//
+//	}
 
+//	@Override
+//	public void removeListener(InvalidationListener arg0) {
+//		// TODO Auto-generated method stub
+//
 	}
 
-	@Override
-	public void removeListener(InvalidationListener arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void update(java.util.Observable arg0, Object arg1) {
-		// TODO Auto-generated method stub
-
-	}
+//	@Override
+//	public void update(java.util.Observable arg0, Object arg1) {
+//		// TODO Auto-generated method stub
+//
+//	}
 
 }
