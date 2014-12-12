21c4591afd5d8742375664270a8386f015c4fc1d
Michael Bredel <michael.bredel@cern.ch>
2014-12-12 10:48:41 +0100

    Formated the methods.

--- app/controllers/IRegistration.java
+++ app/controllers/IRegistration.java
@@ -10,19 +10,16 @@ public class IRegistration implements Observer {
 //	@Override
 //	public void addListener(InvalidationListener arg0) {
 //		// TODO Auto-generated method stub
-//
 //	}
 
 //	@Override
 //	public void removeListener(InvalidationListener arg0) {
 //		// TODO Auto-generated method stub
-//
 //	}
 
-//	@Override
-//	public void update(java.util.Observable arg0, Object arg1) {
-//		// TODO Auto-generated method stub
-//
-//	}
+	@Override
+	public void update(java.util.Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+	}
 
 }
