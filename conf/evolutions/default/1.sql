702dbf7bd567f57d393f0697196f99a02bf421ca
Michael Bredel <michael.bredel@cern.ch>
2014-12-15 11:49:04 +0100

    Some modified entities.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -235,6 +235,10 @@ create table identification (
   constraint ck_identification_identification_type check (identification_type in (0,1)))
 ;
 
+create table identification_type (
+  test                      varchar(255))
+;
+
 create table interview (
   interview_date            timestamp,
   volunteer_id              bigint,
@@ -983,6 +987,8 @@ drop table if exists icgmember_guest_nationality;
 drop table if exists icgmember_guest_degree;
 
 drop table if exists identification;
+
+drop table if exists identification_type;
 
 drop table if exists interview;
 
