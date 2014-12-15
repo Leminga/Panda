fc47cd92be6a8f59f4dda7076172d04f4a248093
Michael Bredel <michael.bredel@cern.ch>
2014-12-15 11:48:20 +0100

    Added a dummy variable to prevent an SQL error.

--- app/models/IdentificationType.java
+++ app/models/IdentificationType.java
@@ -5,9 +5,12 @@ import javax.persistence.Entity;
 import play.db.ebean.Model;
 
 @Entity
-public class IdentificationType extends Model{
+public class IdentificationType extends Model {
 	
 	/** The serialization version identifier. */
 	private static final long serialVersionUID = 1L;
+	
+	/** This is just a dummy variable. */
+	private String test;
 
 }
