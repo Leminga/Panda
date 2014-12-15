997393fb60487da801c8e33f9a412b11cb7ff48f
Michael Bredel <michael.bredel@cern.ch>
2014-12-15 14:46:43 +0100

    Added an Activit process to be deployed at startup.

--- app/activiti/ServicesTimer.java
+++ app/activiti/ServicesTimer.java
@@ -7,27 +7,30 @@ import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.runtime.ProcessInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 
 public class ServicesTimer {
+	/** Logger to log SecurityController events. */
+	private static Logger LOGGER = LoggerFactory.getLogger(ServicesTimer.class);
 	
 	static ProcessEngine processEngine = ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("activiti/resources/activiti.cfg.xml").buildProcessEngine();
 
-		public static void startProcess() {
-
-	
-			RuntimeService runtimeService = processEngine.getRuntimeService();
-			ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("TimeProcess");
-			
-			System.out.println("Die id des Timer-Processes ist aktuell " + processInstance.getId() + " "	+ processInstance.getProcessDefinitionId());
-			
+	public static void startProcess() {
+		RuntimeService runtimeService = processEngine.getRuntimeService();
+		ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("TimeProcess");
+		if (LOGGER.isInfoEnabled()) {
+			LOGGER.info("ServicesTimer started with process instance id: " + processInstance.getId());
 		}
+	}
 		
-		public static void deployProcess(){
-			
-			RepositoryService repositoryService = processEngine.getRepositoryService();
-			repositoryService.createDeployment().addClasspathResource("activiti/resources/diagrams/TimeProcess3.bpmn20.xml").deploy();
+	public static void deployProcess(){
+		RepositoryService repositoryService = processEngine.getRepositoryService();
+		repositoryService.createDeployment().addClasspathResource("activiti/resources/diagrams/TimeProcess3.bpmn20.xml").deploy();
+		if (LOGGER.isInfoEnabled()) {
+			LOGGER.info("ServicesTimer deployed");
 		}
-		
+	}	
 }
--- app/helper/Global.java
+++ app/helper/Global.java
@@ -1,11 +1,14 @@
 package helper;
 
+import activiti.ServicesTimer;
 import play.*;
 
 public class Global extends GlobalSettings {
 	
 	@Override
 	  public void onStart(Application app) {
+		// Deploy Activiti services.
+		ServicesTimer.deployProcess();
 	    Logger.info("Application has started");
 	  }  
 
