d6553a45104053bd928174844486404cfd5ed798
Alessia12 <a.lermer93@gmail.com>
2014-12-29 14:13:49 +0100

    Permission

--- app/models/Permission.java
+++ app/models/Permission.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import play.data.validation.Constraints.Required;
@@ -11,18 +13,20 @@ import play.data.validation.Constraints.Required;
 @Entity
 public class Permission {
 
+	@Id
+	@Required
+	@GeneratedValue
+	private long id;
+	
 	@Required
 	@OneToMany(cascade = CascadeType.ALL)
-	private int permission;
+	private int[] permission;
 
-	public int getPermission() {
+	public int[] getPermission() {
 		return permission;
 	}
 
-	public void setPermission(int permission) {
+	public void setPermission(int[] permission) {
 		this.permission = permission;
 	}
-	
-	
-	
 }
