cf638173de19d4de997396519937dcacbc8e854d
Leminga <markus.s@deflagratio.at>
2015-01-11 15:36:47 +0100

    Update FileHandler.

--- app/controllers/FileHandler.java
+++ app/controllers/FileHandler.java
@@ -5,7 +5,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.util.Base64;
 
 import org.imgscalr.Scalr;
 import org.imgscalr.Scalr.Method;
