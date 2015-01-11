f7564438688489ad8087780149ea12b164827865
Leminga <markus.s@deflagratio.at>
2015-01-11 20:38:07 +0100

    Finished

--- app/controllers/FileHandler.java
+++ app/controllers/FileHandler.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import helper.FileType;
+
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -15,36 +17,101 @@ import javax.swing.filechooser.FileSystemView;
 import javax.xml.bind.DatatypeConverter;
 
 import org.apache.commons.io.FileUtils;
-import org.dom4j.datatype.DatatypeAttribute;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import play.Play;
+import sun.misc.BASE64Encoder;
 
-import com.sun.prism.Image;
+
 
 /*
- * Diese Klasse kümmert sich um die upgeloadeten Files.
+ * This class is used to handle files.
  */
 
 public class FileHandler {
 
-	private static Logger LOGGER = LoggerFactory.getLogger(FileHandler.class);
-
-	public static void createNewFile() throws IOException {
-		// File f = new File("E:"+File.separator,"Test3.txt");
-		// f.createNewFile();
-		// LOGGER.info("File created");
-		// File f = FileSystemView.getFileSystemView().getHomeDirectory();
+	private static Logger LOGGER = LoggerFactory.getLogger(FileHandler.class);	
 	
+	public static boolean savePicture(String vId, String prename, String surename,String event, String base64Code) 
+	{		
+		String path = pathBuilder(vId, prename, surename, event);
+		
+		directoryBuilder(path);
+		
+		saveAsJPG(pictureDecoder(base64Code), path)	;
+		return true;
 	}
-
+	
+	public static String getPicture(String vId, String prename, String surename,String event, FileType fileType) throws IOException
+	{		
+		if(FileType.THUMBNAIL == fileType)
+		{
+		//LOGGER.info(pictureDecode("jpg",pathBuilder(vId, prename, surname, event)+"Photo"+File.separator+"thumbnail.jpg"));
+		return pictureEncode("jpg",pathBuilder(vId, prename, surename, event)+"Photo"+File.separator+"thumbnail.jpg");
+		}
+		else if(FileType.PICTURE == fileType)
+		{		
+		//LOGGER.info(pictureDecode("jpg",pathBuilder(vId, prename, surname, event)+"Photo"+File.separator+"picture.jpg"));		
+	    return pictureEncode("jpg",pathBuilder(vId, prename, surename, event)+"Photo"+File.separator+"picture.jpg");
+		}
+		else if(FileType.PASSPORT == fileType)
+		{
+		return "";
+		}
+		else if(FileType.WAIVER == fileType)
+		{
+		return "";
+		}
+		else
+		{
+		return "";
+		}
+	}
+	
+	
+	public static void deletDirectoriesAndFiles(String path) 
+	{
+		File f = new File(path);
+		
+		try
+			{
+				FileUtils.deleteDirectory(f);
+			}
+		catch (IOException e) 
+			{
+				LOGGER.error("File or Directory could not be deleted! Filename: "+f.getName());
+			}
+		
+		File pf = f.getParentFile();
+		
+		if(pf.isDirectory()){
+			 
+			if(!(pf.list().length>0)){
+	 
+				try 
+				{
+					FileUtils.deleteDirectory(pf);
+					LOGGER.info("Parent "+pf.toString()+" removed");
+				} 
+				catch (IOException e) 
+				{
+					LOGGER.error("File or Directory could not be deleted! "+ pf.getName());
+				}	 
+			}
+			else
+			{	 
+				LOGGER.info("Directory is not empty"+pf.list().length);
+		   }		
+		}
+	}
+	
+	
 	/*
-	 * Build`s the Directory path´s.
+	 * Build the Directory path.
 	 */
-	public static String pathBuilder(String vId, String prename, String surename,String event) {
-		String confPath = Play.application().configuration()
-				.getString("directory.path");
+	private static String pathBuilder(String vId, String prename, String surename,String event) {
+		String confPath = Play.application().configuration().getString("directory.path");
 		String path = FileSystemView.getFileSystemView().getRoots()[0].toString();
 		path += File.separator + confPath +File.separator+ event + File.separator + surename + "_"+ prename + "_" + vId + File.separator;
 		
@@ -54,104 +121,145 @@ public class FileHandler {
 	/*
 	 * Build`s the Directories for Volunteer`s
 	 */	
-	public static void directoryBuilder(String path) {
+	private static void directoryBuilder(String path) {
 		File dir = new File(path);
 
-		if (!dir.exists()) {
-			LOGGER.info("Try to create Directories");
+		if (!dir.exists()) 
+			{
+				LOGGER.info("Try to create Directories");
 
 			if (new File(path).mkdirs() && new File(path + "Passport").mkdir()
 					&& new File(path + "Photo").mkdir()
 					&& new File(path + "Waiver").mkdir()) {
 				LOGGER.info("Directories created");
 			}
-		} else {
-			LOGGER.info("Directory already exist`s" + dir.exists());
-		}
+		} 
+		else 
+			{
+				LOGGER.info("Directory already exist`s" + dir.exists());
+			}
 	}
+	
+	/*
+	 *Encodes a file into the given type.
+	 */	
+	
+	public static String pictureEncode(String type, String path) {
+	        
+			BufferedImage image = null;
+			 try
+			 {
+				 image = ImageIO.read(new File(path));
+			 }
+			 catch (IOException e1)
+			 {
+				
+			 }
+			 
+			String imageString = null;
+	        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+	
+	        try {
+	            ImageIO.write(image, type, bos);
+	            byte[] imageBytes = bos.toByteArray();
+	
+	            BASE64Encoder encoder = new BASE64Encoder();
+	            imageString = encoder.encode(imageBytes);
+	
+	            bos.close();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	        return imageString;
+	    }
 
 	/*
-	 * PictureDecoder erhält einen einen String und erstellt daraus ein Bild.
+	 * PictureDecoder decodes an given String to a BufferedImage.
 	 */
 
-	public static BufferedImage PictureDecoder(String string)
-			throws IOException {
+	private static BufferedImage pictureDecoder(String string)
+	{
 		byte[] pictureByte = DatatypeConverter.parseBase64Binary(string);
-		BufferedImage img = ImageIO.read(new ByteArrayInputStream(pictureByte));
-		LOGGER.info("pasrsing Image");
-		return img;
+		BufferedImage img;
+		try
+		{
+			img = ImageIO.read(new ByteArrayInputStream(pictureByte));
+			return img;
+		} 
+		catch (IOException e) 
+		{
+			LOGGER.error("Could not decode byte[] to Image!");
+		}		
+		return null;
 	}
-
+		
 	/*
-	 * PictureEncoder erhält ein Image und erstellt daraus eine Zeichenkette.
-	 */
-	private static String PictureEncoder(String filePath) throws IOException {
-		BufferedImage originalImage = ImageIO.read(new File(filePath));
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		ImageIO.write(originalImage, "jpg", baos);
-		baos.flush();
-		byte[] imageInByte = baos.toByteArray();
-		baos.close();
-		return imageInByte.toString();
-	}
-
-	public static boolean SaveAsJPG(BufferedImage bImage, String savePath)
-			throws IOException {
-		File outputfile = new File(savePath+"Photo"+File.separator+"saved.jpg");
-		outputfile.createNewFile();
+	* Saves BufferedImage as saved.jpg and as thumbnail.jpg to given path.
+	*/
+	
+	private static boolean saveAsJPG(BufferedImage bImage, String savePath)
+		{
+		File f = new File(savePath+"Photo"+File.separator+"picture.jpg");
+		File tf = new File(savePath+"Photo"+File.separator+"thumbnail.jpg");
 		
-		if(bImage == null){
-			LOGGER.info("Image is null");
-		}
-		ImageIO.write(bImage, "jpg", outputfile);
-		
-		LOGGER.info(savePath+outputfile.toString()+bImage);
-		
-		BufferedImage thumbImg = Scalr.resize(bImage, Method.QUALITY,Mode.AUTOMATIC,50,50, Scalr.OP_ANTIALIAS);
-		//convert bufferedImage to outpurstream 
-		ByteArrayOutputStream os = new ByteArrayOutputStream();
-		ImageIO.write(thumbImg,"jpg",os);  
-	  		  //or wrtite to a file
-		  
-		File f = new File(savePath+"Photo"+File.separator+"thumbnail.jpg");
-		 		  
-		ImageIO.write(thumbImg, "jpg", f);	
-		
+		if(bImage == null)
+			{
+				LOGGER.info("Image is null");
+			}			
+		else
+			{
+			BufferedImage bThumbnailImage = Scalr.resize(bImage, Method.QUALITY,Mode.FIT_EXACT,Play.application().configuration().getInt("thumbnail.height"),Play.application().configuration().getInt("thumbnail.width"), Scalr.OP_ANTIALIAS);
+			LOGGER.info(Play.application().configuration().getInt("thumbnail.height")+" "+Play.application().configuration().getInt("thumbnail.width"));
+			
+			try
+				{
+					ImageIO.write(bImage, "jpg", f);
+					ImageIO.write(bThumbnailImage, "jpg", tf);
+				} 
+			catch (IOException e)
+				{
+					LOGGER.error("Writing BufferedImage to filesystem failed!");
+				}
+			}
 		return true;
 	}
 	
-	public static void thumbnailBuilder(BufferedImage img,String savePath) throws IOException
-		{
-		 BufferedImage thumbImg = Scalr.resize(img, Method.QUALITY,Mode.AUTOMATIC,50,50, Scalr.OP_ANTIALIAS);
-		 //convert bufferedImage to outpurstream 
-		 ByteArrayOutputStream os = new ByteArrayOutputStream();
-		 ImageIO.write(thumbImg,"jpg",os);  
-	  
-		  //or wrtite to a file
-		  
-		  File f = new File(savePath+"Photo"+File.separator+"thumbnail.jpg");
-		  		  
-		  ImageIO.write(thumbImg, "jpg", f);	  
-	  
-		} 	
-	public static void deletDirectoriesAndFiles(String path) throws IOException
-	{
-		File f = new File(path);	
-		FileUtils.deleteDirectory(f);
-		
-		File pf = f.getParentFile();
-		
-		if(pf.isDirectory()){
-			 
-			if(!(pf.list().length>0)){
-	 
-				FileUtils.deleteDirectory(pf);
-				LOGGER.info("Parent "+pf.toString()+" removed");
-	 
-			}else{
-	 
-				LOGGER.info("Directory is not empty"+pf.list().length);
-		   }		
-		}
+	
+	
+//	private static void thumbnailBuilder(BufferedImage img,String savePath)
+//		{
+//		 BufferedImage thumbImg = Scalr.resize(img, Method.QUALITY,Mode.AUTOMATIC,Play.application().configuration().getInt("thumbnail.height"),Play.application().configuration().getInt("thumbnail.width"), Scalr.OP_ANTIALIAS);
+//		 File f = new File(savePath+"Photo"+File.separator+"thumbnail.jpg");
+//		 
+//		 ByteArrayOutputStream os = new ByteArrayOutputStream();
+//		 try 
+//		 {
+//			ImageIO.write(thumbImg,"jpg",os);
+//			ImageIO.write(thumbImg, "jpg", f);
+//		 } 
+//		 catch (IOException e)
+//		 {
+//			LOGGER.error("Could not write to File! Path: "+savePath );
+//		 }  	  
+//	  
+//		}
+//		private static String pictureEncoder(String filePath)
+//		{
+//			BufferedImage originalImage;
+//			try 
+//			{
+//				originalImage = ImageIO.read(new File(filePath));
+//				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//				ImageIO.write(originalImage, "jpg", baos);
+//				baos.flush();
+//				byte[] imageInByte = baos.toByteArray();
+//				baos.close();
+//				return imageInByte.toString();
+//			} 
+//			catch (IOException e)
+//			{
+//				LOGGER.error("Could not read from File: "+filePath);
+//			}
+//			return "";		
+//		}	
 	}
-}
