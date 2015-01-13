482fcf51ffd291cb69f9071a394a4f0d243d33c6
bernadettewieser <wieserbernadette@hotmail.com>
2015-01-13 10:17:43 +0100

    added labels in eventSpecific.html and added data binding in eventSpecificCtrl

--- client/frontend/eventSpecific/EventSpecificFormCtrl.js
+++ client/frontend/eventSpecific/EventSpecificFormCtrl.js
@@ -5,7 +5,12 @@ angular.module('Panda')
             var self = this;
 
             DataService.get().then(function (response) {
-                self.user = response.data;
+                self.user = response.data.user;
+                self.description = response.data.description;
+                self.labels = response.data.labels;
+                self.description = response.data.description;
+                self.values = response.data.values;
+
             });
 
             self.saveData = function (user) {
