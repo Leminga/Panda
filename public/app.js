720bdb6fe290425a8b0319cfd1fbdb68dadf8672
Lukas Schmalzbauer <schmalzbauerlukas94@users.noreply.github.com>
2015-01-13 18:22:16 +0100

    CoreData now parses the incoming JSON object and is able to handle atleast some data

--- public/app.js
+++ public/app.js
@@ -13092,7 +13092,7 @@ angular.module('Panda')
             DataService.get().then(function (response) {
                     self.tmp_picture = "";
                     self.profilePicture = response.data.picture;
-                    self.user = response.data.user;
+                    self.user = JSON.parse(response.data.volunteer);
                     self.labels = response.data.labels;
                     self.description = response.data.description;
                     self.values = response.data.values;
