f98ba94eb9f8ce0c3b4f6a95bb30c5bbef982f28
mikevinmike <michael.dabernig@gmail.com>
2015-01-13 12:33:00 +0100

    send register dates in unix time

--- client/frontend/registration/RegisterService.js
+++ client/frontend/registration/RegisterService.js
@@ -2,6 +2,7 @@ angular.module('Panda')
     .factory('RegisterService', ['$http', function ($http) {
         return {
             register:function(user){
+                user.dateOfBirth = user.dateOfBirthHumanized.getTime();
                 return $http.post("/register",user);
             }
         };
